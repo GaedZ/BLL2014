08d0fd7ff8287a27a42e101ad1352cb3e6d9211f
RexUmbrarum <20england11@gmail.com>
2013-12-31 16:03:17 +0100

    Initial Commit

---
+++ BLL_Abitur/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BLL_Abitur
+//
+//  Created by Georg Zänker on 31.12.13.
+//  Copyright (c) 2013 Georg Zänker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ BLL_AbiturTests/BLL_AbiturTests.m
@@ -0,0 +1,34 @@
+//
+//  BLL_AbiturTests.m
+//  BLL_AbiturTests
+//
+//  Created by Georg Zänker on 31.12.13.
+//  Copyright (c) 2013 Georg Zänker. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BLL_AbiturTests : XCTestCase
+
+@end
+
+@implementation BLL_AbiturTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
